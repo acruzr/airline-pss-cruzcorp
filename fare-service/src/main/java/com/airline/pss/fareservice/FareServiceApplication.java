ae4ac5be9bfaa820a2a3a637f15b687e736e3d4e
acruz <acruz@ooyala.com>
2018-08-02 17:24:43 -0500

    initial fare service project

---
+++ fare-service/src/main/java/com/airline/pss/fareservice/FareServiceApplication.java
@@ -0,0 +1,12 @@
+package com.airline.pss.fareservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FareServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FareServiceApplication.class, args);
+	}
+}
