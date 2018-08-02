a85b612a9c8dd018e7f3a3cb2e0300ead9a56adc
acruz <acruz@ooyala.com>
2018-08-02 17:29:40 -0500

    initial checkin service project

---
+++ checkin-service/src/main/java/com/airline/pss/checkinservice/CheckinServiceApplication.java
@@ -0,0 +1,12 @@
+package com.airline.pss.checkinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckinServiceApplication.class, args);
+	}
+}
