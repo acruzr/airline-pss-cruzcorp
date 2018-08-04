bd934ab5ba6b87bc9bc5cf7a027ab54feeb9813d
acruz <acruz@ooyala.com>
2018-08-04 18:13:38 -0500

    added configuration to flush properties from the config server on each service of the system

---
+++ fare-service/src/main/java/com/airline/pss/fareservice/controllers/HelloController.java
@@ -0,0 +1,19 @@
+package com.airline.pss.fareservice.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class HelloController {
+
+  @Value("${message}")
+  private String testProperty;
+
+  @GetMapping("/test")
+  public String testFareService(){
+    return testProperty;
+  }
+}
